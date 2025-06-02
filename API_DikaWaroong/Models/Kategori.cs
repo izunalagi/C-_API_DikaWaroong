a6de9690efda89dbf99ce0dbf7d5bf613189b3f4
farel_aja <116055126+izunalagi@users.noreply.github.com>
2025-06-02 20:22:21 +0700

    hapus all

--- API_DikaWaroong/Models/Kategori.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API_DikaWaroong.Models
-{
-    public class Kategori
-    {
-        public int IdKategori { get; set; }
-        public string NamaKategori { get; set; } = string.Empty;
-    }
-}
